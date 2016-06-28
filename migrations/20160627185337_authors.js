ff3e2a058d6c37dd6960771e527ae442556497bd
Patrick Richardson <setfloat@gmail.com>
2016-06-28 10:11:36 -0700

    max id from authors table

--- migrations/20160627185337_authors.js
+++ migrations/20160627185337_authors.js
@@ -2,7 +2,7 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable('authors', (table) => {
-    table.increments('id');
+    table.increments();
     table.string('first_name').notNullable().defaultTo('');
     table.string('last_name').notNullable().defaultTo('');
     table.text('biography').notNullable().defaultTo('');
